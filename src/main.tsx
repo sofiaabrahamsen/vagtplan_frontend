17febe551a503ea148edf074cd82f3e248fe7bac
Cathrine <achc.sfi@outlook.com>
2025-12-13 23:01:15 +0100

    added logging

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
+import * as Sentry from "@sentry/react";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/dot-notation
+const SENTRYDSN: string = import.meta.env["VITE_SENTRY_DSN"];
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/dot-notation
+const VITE_API_URL: string = import.meta.env["VITE_API_URL"];
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+Sentry.init({
+  dsn: SENTRYDSN,
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration()
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost",VITE_API_URL],
+  // Enable logs to be sent to Sentry
+  enableLogs: true
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/services/loginClient.ts
+++ src/services/loginClient.ts
@@ -1,15 +1,24 @@
 import axios from "axios";
+import * as Sentry from '@sentry/react';
 
 // eslint-disable-next-line @typescript-eslint/dot-notation
 const VITE_LOGIN_API_URL = import.meta.env["VITE_LOGIN_API_URL"] as string;
 
 export const loginClient = {
+ 
   async signIn(username: string, password: string): Promise<string> {
-    const response = await axios.post(VITE_LOGIN_API_URL, {
+     try{
+      const response = await axios.post(VITE_LOGIN_API_URL, {
       username,
       password,
     });
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return response.data; // JWT token string
-  },
+    }catch(error:unknown){
+    // // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      Sentry.captureException(error);
+      throw error;
+    } 
+    
+  }  
 };
