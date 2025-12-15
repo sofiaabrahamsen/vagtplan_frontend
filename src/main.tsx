543750529a0718580600d9af87115bd6237f6fc0
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2025-12-15 08:40:32 +0100

    ReactQueryDevTools removed from dev

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
@@ -51,7 +50,6 @@ createRoot(document.getElementById('root')!).render(
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-        <ReactQueryDevtools/>
       </QueryClientProvider>
     </ChakraProvider>
 )
